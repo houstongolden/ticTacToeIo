d15b67d93bcb7283595c2164b31b5d4ad830847d
Randy Lee <randyrlee@gmail.com>
2013-10-09 17:44:27 -0700

    just commiting

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,88 +1,99 @@
 'use strict';
 
 angular.module('newTicApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', ['$scope', 'angularFire', function ($scope, angularFire) {
 
-  	$scope.ticTacToe = [[{value:""},{value:""},{value:""}],
+  	// calls to firebase
+  	var database = new Firebase("https://fire-base-tictactoe.firebaseio.com/room/");
+  	$scope.room={};
+  	// promise delays response to firebase
+  	var promise = angularFire(database, $scope, "room");
+
+  	promise.then(function(){
+  		$scope.room={
+  			ticTacToe: [[{value:""},{value:""},{value:""}],
   						[{value:""},{value:""},{value:""}],
-  						[{value:""},{value:""},{value:""}]];
+  						[{value:""},{value:""},{value:""}]],
+			turnNum: 0,
+			// players: [player1, player2]
+			// player1: Math.floor(Math.random()*1001),
+			// player2: Math.floor(Math.random()*1001),
+			// if(player1 == player2) {
+			// 	player2 + 1;
+			// }
+			};
 
-	var turnNum = 0;
+		$scope.clickBox = function(cell) {
 
-	$scope.clickBox = function(cell) {
-
-		var letter;
-
-		if(cell.value != "")
-			return;
-		// prevents overwriting
-
-		if(turnNum % 2 == 0)
-			letter = "X";
-		else
-			letter = "O";
-		// alternates turns
-
-		cell.value = letter;
-
-		++turnNum;
-		if(turnNum == 9)
-			alert("It's a tie!");
-
-		$scope.wins(cell);
-
-	}
-	// win conditions
-	$scope.wins = function() {
-
-		for(var x=0; x<=2; ++x) {
-		if($scope.ticTacToe[0][x].value == $scope.ticTacToe[1][x].value &&
-			$scope.ticTacToe[1][x].value == $scope.ticTacToe[2][x].value &&
-			$scope.ticTacToe[0][x].value != "") {
-			if($scope.ticTacToe[0][x].value == "X")
-				$scope.xWin = true;
+			// prevents overwriting
+			if(cell.value != "")
+				return;
+			
+			// alternates turns
+			if($scope.room.turnNum % 2 == 0)
+				cell.value = "X";
 			else
-				$scope.yWin = true;
-			}
-		if($scope.ticTacToe[x][0].value == $scope.ticTacToe[x][1].value &&
-			$scope.ticTacToe[x][1].value == $scope.ticTacToe[x][2].value &&
-			$scope.ticTacToe[x][0].value != "") {
-			if($scope.ticTacToe[x][0].value == "X")
-				$scope.xWin = true;
-			else
-				$scope.yWin = true;
-			}
-		
-		// if($scope.ticTacToe[0][x].value == $scope.ticTacToe[1][x+1].value &&
-		// 	$scope.ticTacToe[1][x+1].value == $scope.ticTacToe[2][x+2].value &&
-		// 	$scope.ticTacToe[0][x].value != "") {
-		// 	if($scope.ticTacToe[0][x].value == "X")
-		// 		alert("X Wins");
-		// 	else
-		// 		alert("O Wins");
-		// 	}
-		// 	console.log(x);
-		// if($scope.ticTacToe[0][x].value == $scope.ticTacToe[1][x-1].value &&
-		// 	$scope.ticTacToe[1][x-1].value == $scope.ticTacToe[2][x-2].value &&
-		// 	$scope.ticTacToe[0][x].value != "") {
-		// 	if($scope.ticTacToe[0][x].value == "X")
-		// 		alert("X Wins");
-		// 	else
-		// 		alert("O Wins");
-		// 	}
+				cell.value = "O";
+
+			++$scope.room.turnNum;
+			if($scope.room.turnNum == 9)
+				alert("It's a tie!");
+
+			$scope.wins(cell);
+
 		}
-	}
+		// win conditions
+		$scope.wins = function() {
 
-	// resets game
-	$scope.resetGame = function() {
-		var tic = $scope.ticTacToe;
+			for(var x=0; x<=2; ++x) {
+			if($scope.room.ticTacToe[0][x].value == $scope.room.ticTacToe[1][x].value &&
+				$scope.room.ticTacToe[1][x].value == $scope.room.ticTacToe[2][x].value &&
+				$scope.room.ticTacToe[0][x].value != "") {
+				if($scope.room.ticTacToe[0][x].value == "X")
+					$scope.xWin = true;
+				else
+					$scope.yWin = true;
+				}
+			if($scope.room.ticTacToe[x][0].value == $scope.room.ticTacToe[x][1].value &&
+				$scope.room.ticTacToe[x][1].value == $scope.room.ticTacToe[x][2].value &&
+				$scope.room.ticTacToe[x][0].value != "") {
+				if($scope.room.ticTacToe[x][0].value == "X")
+					$scope.xWin = true;
+				else
+					$scope.yWin = true;
+				}
+			
+			// if($scope.ticTacToe[0][x].value == $scope.ticTacToe[1][x+1].value &&
+			// 	$scope.ticTacToe[1][x+1].value == $scope.ticTacToe[2][x+2].value &&
+			// 	$scope.ticTacToe[0][x].value != "") {
+			// 	if($scope.ticTacToe[0][x].value == "X")
+			// 		alert("X Wins");
+			// 	else
+			// 		alert("O Wins");
+			// 	}
+			// 	console.log(x);
+			// if($scope.ticTacToe[0][x].value == $scope.ticTacToe[1][x-1].value &&
+			// 	$scope.ticTacToe[1][x-1].value == $scope.ticTacToe[2][x-2].value &&
+			// 	$scope.ticTacToe[0][x].value != "") {
+			// 	if($scope.ticTacToe[0][x].value == "X")
+			// 		alert("X Wins");
+			// 	else
+			// 		alert("O Wins");
+			// 	}
+			}
+		}
 
-		for(var r in tic)
-			for(var c in tic[r]) 
-				tic[r][c].value = '';
-		turnNum = 0;
-		$scope.xWin = false;
-		$scope.yWin = false;
-		$scope.showDetails = false;
-	}
-});
+		// resets game
+		$scope.resetGame = function() {
+			var tic = $scope.room.ticTacToe;
+
+			for(var r in tic)
+				for(var c in tic[r]) 
+					tic[r][c].value = '';
+			$scope.room.turnNum = 0;
+			$scope.xWin = false;
+			$scope.yWin = false;
+			$scope.showDetails = false;
+		}
+	});
+}]);
